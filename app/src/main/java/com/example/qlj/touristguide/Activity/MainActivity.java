6c0b2f3b4fe872469d673261894f0eea61d2e452
qianlinjun <1025610469@qq.com>
2017-04-17 22:03:40 +0800

    4_17版本，完成了基本框架。景点列表、详细信息显示还没有完成（可以和王凯确认一下最后展示信息）

--- app/src/main/java/com/example/qlj/touristguide/Activity/MainActivity.java
+++ app/src/main/java/com/example/qlj/touristguide/Activity/MainActivity.java
@@ -1,62 +1,52 @@
 package com.example.qlj.touristguide.Activity;
 
-import android.content.ContentValues;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.Rect;
-import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.IdRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import com.amap.api.location.AMapLocation;
-import com.amap.api.location.AMapLocationClient;
-import com.amap.api.location.AMapLocationClientOption;
-import com.amap.api.location.AMapLocationListener;
-import com.amap.api.location.DPoint;
-import com.amap.api.maps2d.LocationSource;
-import com.example.qlj.touristguide.Database_SQLite.MyDataBasehelp;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import com.example.qlj.touristguide.Fragment.FragAdapter;
 import com.example.qlj.touristguide.Fragment.Fragment_Map;
+import com.example.qlj.touristguide.Fragment.Fragment_Me;
 import com.example.qlj.touristguide.Fragment.Fragment_SightseeingList;
-import com.example.qlj.touristguide.Fragment.Fragment_TraceManager;
+import com.example.qlj.touristguide.Fragment.Fragment_Share;
 import com.example.qlj.touristguide.R;
-import com.example.qlj.touristguide.TraceAnalysis.DBScan;
-import com.example.qlj.touristguide.TraceAnalysis.LocPoint;
+import com.getbase.floatingactionbutton.FloatingActionButton;
 
 
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.List;
 
-import static com.amap.api.location.CoordinateConverter.calculateLineDistance;
 
-public class MainActivity extends FragmentActivity{
+public class MainActivity extends FragmentActivity implements RadioGroup.OnCheckedChangeListener, ViewPager.OnPageChangeListener {
 
+    private RadioGroup mRadioGroup;
     //viewPage+Fragment
     private ViewPager viewPager;
     List<Fragment> fragmentList;
     private FragAdapter adapter;
-    Fragment_Map frag1;
-    Fragment_SightseeingList frag2;
-    Fragment_TraceManager frag3;
-    int currenttab=-1;
+    private Fragment_Map frag1;//地图
+    private Fragment_SightseeingList frag2;//旅游景点
+    private Fragment_Share frag3;//共享
+    private Fragment_Me frag4;//账户管理
+    private int currenttab=-1;
+
+    //动态按钮效果
+//    FloatingActionButton button = (FloatingActionButton) findViewById(R.id.setter);
+//    button.setSize(FloatingActionButton.SIZE_MINI);
+//    button.setColorNormalResId(R.color.pink);
+//    button.setColorPressedResId(R.color.pink_pressed);
+//    button.setIcon(R.drawable.ic_fab_star);
+//    button.setStrokeVisible(false);
+
+//    Button bt_Map;
+//    Button bt_TouristInfor;
+//    Button bt_TraceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,48 +54,146 @@ public class MainActivity extends FragmentActivity{
         //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//取消状态栏
         setContentView(R.layout.layout_activitymain);
         initView();
+//        Log.d("currenpath",getApplicationContext().getFilesDir().getAbsolutePath());
+//        Log.d("packagepath",getApplicationContext().getPackageResourcePath());
+//        Log.d("databasepath",getApplicationContext().getDatabasePath("user").getAbsolutePath());
     }//onCreate
 
     /*----------实现函数------------*/
     //初始化pageview和fragment
     private void initView()
     {
+        mRadioGroup=(RadioGroup)findViewById(R.id.radioGroup);
+        mRadioGroup.setOnCheckedChangeListener(this);
         fragmentList=new ArrayList<Fragment>();
         frag1=new Fragment_Map();//地图Fragment
         frag2=new Fragment_SightseeingList();//旅游景点Fragment
-        frag3=new Fragment_TraceManager();//轨迹管理Fragment
+        frag3=new Fragment_Share();//照片日记Fragment
+        frag4=new Fragment_Me();
         fragmentList.add(frag1);
         fragmentList.add(frag2);
         fragmentList.add(frag3);
+        fragmentList.add(frag4);
         adapter=new FragAdapter(getSupportFragmentManager(), fragmentList);
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         viewPager.setAdapter(adapter);
         viewPager.setCurrentItem(0);
+        viewPager.setOnPageChangeListener(this);
     }
+
+    /**
+     * 按钮的没选中显示的图标
+     */
+    private int[] unselectedIconIds = {
+            R.drawable.icon_umap_40,
+            R.drawable.icon_utourism_40,
+            R.drawable.icon_ushare_40,
+            R.drawable.icon_umanager_40};
+    /**
+     * 按钮的选中显示的图标
+     */
+    private int[] selectedIconIds = {
+            R.drawable.icon_map_40,
+            R.drawable.icon_tourism_40,
+            R.drawable.icon_share_40,
+            R.drawable.icon_manager_40};
+
+    //radio button切换函数
+    private void selectPage(int position) {
+        // 将所有的tab的icon变成灰色的
+        for (int i = 0; i < mRadioGroup.getChildCount(); i++) {
+            Drawable gray = getResources().getDrawable(unselectedIconIds[i]);
+            // 不能少，少了不会显示图片
+            gray.setBounds(0, 0, gray.getMinimumWidth(),
+                    gray.getMinimumHeight());
+            RadioButton child = (RadioButton) mRadioGroup.getChildAt(i);
+            child.setCompoundDrawables(null, gray, null, null);
+            child.setTextColor(getResources().getColor(R.color.divider));
+        }
+        // 切换页面
+        viewPager.setCurrentItem(position, false);
+        // 改变图标
+        Drawable yellow = getResources().getDrawable(selectedIconIds[position]);
+        yellow.setBounds(0, 0, yellow.getMinimumWidth(),
+                yellow.getMinimumHeight());
+        RadioButton select = (RadioButton) mRadioGroup.getChildAt(position);
+        select.setCompoundDrawables(null, yellow, null, null);
+        select.setTextColor(getResources().getColor(R.color.primaryDark));
+    }
+
+
+    //RadioGroup 按钮点击事件监听
+    @Override
+    public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+        switch (checkedId) {
+            case R.id.bt_map: // map radiobt
+                selectPage(0);
+                break;
+            case R.id.bt_touristinfor: // 景点 radiobt
+                selectPage(1);
+                break;
+            case R.id.bt_share: // 分享 radiobt
+                selectPage(2);
+                break;
+            case R.id.btn_me: // 个人中心 radiobt
+                selectPage(3);
+                break;
+        }
+    }
+
+
+    //viewpage页面改变监听
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        selectPage(position);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+
 
 
     /*------------System override------------------*/
     @Override
     protected void onStart() {
         super.onStart();
-        Log.e("map","onStart");
+        Log.e("main","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("main","onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("main","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("main","onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.e("main","onDestroy");
     }
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-       // mapView.onSaveInstanceState(outState);//保存地图当前的状态
     }
 }
---
+++ app/src/main/java/com/example/qlj/touristguide/Fragment/Fragment_Share.java
@@ -0,0 +1,27 @@
+package com.example.qlj.touristguide.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.qlj.touristguide.R;
+
+/**
+ * Created by Qljqian on 2017/4/17.
+ */
+
+public class Fragment_Share extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.layout_share, container, false);
+        return view;
+    }
+}
